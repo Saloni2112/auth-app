ac04cee10bfd25abbcde64660b37ea4796d2c817
Saloni2112 <168178333+Saloni2112@users.noreply.github.com>
2025-05-14 22:16:56 +0530

    Add env variable to api acess

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,11 +1,17 @@
+
 import axios from "axios"
+
+const API_BASE = import.meta.env.VITE_API_URL;
+
+console.log("API_BASE:", API_BASE); // Should show "/api"
+
 const register = async(FormData) => {
-    const response = await axios.post("/api/user/register", FormData)
+    const response = await axios.post(`${API_BASE}/user/register`, FormData)
    localStorage.setItem("user", JSON.stringify(response.data))
     return response.data;
 }
 const login = async(FormData) => {
-    const response = await axios.post("/api/user/login", FormData)
+    const response = await axios.post(`${API_BASE}/user/login`, FormData)
    localStorage.setItem("user", JSON.stringify(response.data))
     return response.data;
 }
